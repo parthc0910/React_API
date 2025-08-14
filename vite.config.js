eca40a434270e73f423118fda28e073f8bc98a62
parthc0910 <parth.c0910@gmail.com>
2025-08-14 20:02:02 +0530

    Update homepage and deployment scripts in package.json; set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/TMBD-APP/', // Set base for GitHub Pages
   plugins: [react(), tailwindcss()],
-  base: '/',
 })
